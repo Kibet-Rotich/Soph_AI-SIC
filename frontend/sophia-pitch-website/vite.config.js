40c6c58178e929df14b6fde3b9b3179aadde39cd
Kibet-Rotich <kibet.rotich2022@students.jkuat.ac.ke>
2026-07-05 06:19:04 +0300

    added basename to vite config

--- frontend/sophia-pitch-website/vite.config.js
+++ frontend/sophia-pitch-website/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/SIC/',
+  base: '/Soph_AI-SIC/',
   plugins: [
     react(),
     tailwindcss()
